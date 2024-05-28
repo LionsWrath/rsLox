dd344a08fa9752388033ef0827755631ad1f5f05
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-05-28 14:01:54 -0300

    visit for expr

--- src/tools/interpreter.rs
+++ src/tools/interpreter.rs
@@ -6,12 +6,19 @@ pub struct Interpreter;
 
 impl ExprVisitor<Literal> for Interpreter {
 
-    fn visit_literal(&mut self, l: &Literal) -> Literal {
-        return l;
+    fn visit_expr(&mut self, e: &Expr) -> Literal {
+        match e {
+            Expr::UNARY(u) => self.visit_unary(&u),
+            Expr::BINARY(b) => self.visit_binary(&b),
+            Expr::GROUPING(g) => self.visit_grouping(&g),
+            Expr::LITERAL(l) => self.visit_literal(&l),
+            Expr::COMMA(c) => self.visit_comma(&c),
+            Expr::TERNARY(t) => self.visit_ternary(&t),
+        } 
     }
 
-    fn visit_expr(&mut self, e: &Expr) -> Literal {
-        unimplemented!();
+    fn visit_literal(&mut self, l: &Literal) -> Literal {
+        return l;
     }
 
     fn visit_unary(&mut self, u: &Unary) -> Literal {
@@ -20,7 +27,7 @@ impl ExprVisitor<Literal> for Interpreter {
         match (u.op.kind, r) {
             (TokenType::MINUS, Literal::NUMBER(val)) => return Literal::new(-val),
             (TokenType::BANG, Literal::BOOL(val)) => return Literal::new(!val),
-            _ => panic!("TODO: fix this"),
+            _ => panic!("TODO"),
         }
     }
 
